2b5f7710ccbb88456b7b2da37425175203155a9b
Гарматенко Иван Анатольевич <ivan.garmatenko@sbermarket.ru>
2023-12-18 20:13:33 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,43 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(original string) (string, error) {
+	var result strings.Builder
+	var previousChar rune
+
+	if len(original) == 0 {
+		return "", nil
+	}
+
+	for _, char := range original {
+		repeatCount, convError := strconv.Atoi(string(char))
+
+		if previousChar == 0 && convError == nil {
+			return "", ErrInvalidString
+		}
+
+		if convError == nil && previousChar != 0 {
+			result.WriteString(strings.Repeat(string(previousChar), repeatCount))
+			previousChar = 0
+			continue
+		}
+
+		if previousChar != 0 {
+			result.WriteString(string(previousChar))
+		}
+
+		previousChar = char
+	}
+
+	if previousChar != 0 {
+		result.WriteString(string(previousChar))
+	}
+
+	return result.String(), nil
 }
