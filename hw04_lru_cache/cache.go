5c99de8e2d64e0c56ebf719e0feb39d3769e8598
Гарматенко Иван Анатольевич <ivan.garmatenko@sbermarket.ru>
2024-04-26 18:25:00 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -9,8 +11,9 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
+	Cache
 
+	mu       sync.Mutex
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
@@ -22,4 +25,59 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (lru *lruCache) Set(key Key, value interface{}) bool {
+	lru.mu.Lock()
+	defer lru.mu.Unlock()
+
+	for k, item := range lru.items {
+		if k == key {
+			item.Value = value
+			lru.queue.MoveToFront(item)
+			return true
+		}
+	}
+
+	if lru.capacity == len(lru.items) {
+		lru.purge()
+	}
+
+	i := lru.queue.PushFront(value)
+	lru.items[key] = i
+
+	return false
+}
+
+func (lru *lruCache) Get(key Key) (interface{}, bool) {
+	lru.mu.Lock()
+	defer lru.mu.Unlock()
+
+	for k, item := range lru.items {
+		if k == key {
+			lru.queue.MoveToFront(item)
+			return item.Value, true
+		}
+	}
+
+	return nil, false
+}
+
+func (lru *lruCache) purge() {
+	tail := lru.queue.Back()
+
+	for key, item := range lru.items {
+		if item == tail {
+			delete(lru.items, key)
+			lru.queue.Remove(tail)
+		}
+	}
+}
+
+func (lru *lruCache) Clear() {
+	lru.mu.Lock()
+	defer lru.mu.Unlock()
+
+	lru.queue = NewList()
+	lru.items = make(map[Key]*ListItem, lru.capacity)
 }
