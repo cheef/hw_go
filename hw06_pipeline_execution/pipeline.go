1d02723d8a5f0a79483545f748dbddb63ea830b1
Гарматенко Иван Анатольевич <ivan.garmatenko@sbermarket.ru>
2024-05-18 19:03:45 +0300

    HW06 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,39 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	result := in
+
+	for _, stage := range stages {
+		result = stage(wrap(result, done))
+	}
+
+	return result
+}
+
+func wrap(in In, done In) Out {
+	ch := make(Bi)
+
+	go func() {
+		defer close(ch)
+
+		for {
+			select {
+			case <-done:
+				return
+			case value, ok := <-in:
+				if !ok {
+					return
+				}
+
+				select {
+				case <-done:
+					return
+				default:
+					ch <- value
+				}
+			}
+		}
+	}()
+
+	return ch
 }
--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -90,4 +90,20 @@ func TestPipeline(t *testing.T) {
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
+
+	t.Run("empty incoming channel", func(t *testing.T) {
+		in := make(Bi)
+
+		go func() {
+			<-time.After(sleepPerStage)
+			close(in)
+		}()
+
+		result := make([]string, 0, 10)
+		for s := range ExecutePipeline(in, nil, stages...) {
+			result = append(result, s.(string))
+		}
+
+		require.Equal(t, []string{}, result)
+	})
 }
