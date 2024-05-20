41488e5577aaa9900c9acb16e56abfe1fb7be5ee
Гарматенко Иван Анатольевич <ivan.garmatenko@sbermarket.ru>
2024-05-21 02:55:34 +0300

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,122 @@ package main
 
 import (
 	"errors"
+	"io"
+	"math"
+	"os"
+
+	pb "github.com/cheggaaa/pb/v3"
 )
 
 var (
+	ErrFileNotFound          = errors.New("file not found")
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrSamePath              = errors.New("trying to copy to the same path")
+)
+
+const (
+	bufferSize = 1
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
-	return nil
+	from, err := validateFileToCopy(fromPath, toPath, offset)
+	if err != nil {
+		return err
+	}
+	defer from.Close()
+
+	to, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer to.Close()
+
+	return copyByBuffer(from, to, bufferSize, offset, limit)
+}
+
+func validateFileToCopy(fromPath, toPath string, offset int64) (f *os.File, e error) {
+	from, err := os.Open(fromPath)
+	if err != nil {
+		return nil, ErrFileNotFound
+	}
+
+	info, err := from.Stat()
+
+	if err != nil || info.Size() == 0 {
+		return nil, ErrUnsupportedFile
+	}
+
+	if info.Size() <= offset {
+		return nil, ErrOffsetExceedsFileSize
+	}
+
+	info2, err := os.Lstat(toPath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return from, nil
+		}
+
+		return nil, err
+	}
+
+	if os.SameFile(info, info2) {
+		return nil, ErrSamePath
+	}
+
+	return from, nil
+}
+
+func copyByBuffer(from *os.File, to *os.File, bufferSize, startOffset, limit int64) error {
+	buffer := make([]byte, bufferSize)
+	position := startOffset
+	lastPosition := limit + startOffset
+
+	bar, err := getProgressBar(from, startOffset, limit)
+	if err != nil {
+		return err
+	}
+
+	defer bar.Finish()
+
+	for {
+		if limit != 0 && position >= lastPosition {
+			return nil
+		}
+
+		read, err := from.ReadAt(buffer, position)
+
+		if err != nil && err != io.EOF {
+			return err
+		}
+
+		if read == 0 {
+			return nil
+		}
+
+		if _, err := to.Write(buffer); err != nil {
+			return err
+		}
+
+		bar.Increment()
+		position += bufferSize
+	}
+}
+
+func getProgressBar(f *os.File, offset, limit int64) (*pb.ProgressBar, error) {
+	info, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	barSize := info.Size()
+	barSize -= offset
+
+	if limit != 0 {
+		barSize = int64(math.Min(float64(barSize), float64(limit)))
+	}
+
+	bar := pb.StartNew(int(barSize))
+
+	return bar, nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,79 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	const (
+		destination = "/tmp/input.txt"
+	)
+
+	t.Run("when file does not exist", func(t *testing.T) {
+		err := Copy("./test.txt", destination, 0, 0)
+
+		require.Equal(t, ErrFileNotFound, err)
+	})
+
+	t.Run("when valid file provided", func(t *testing.T) {
+		err := Copy("testdata/input.txt", destination, 0, 0)
+
+		require.Nil(t, err)
+	})
+
+	t.Run("when same file provided", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "testdata/input.txt", 0, 0)
+
+		require.Equal(t, ErrSamePath, err)
+	})
+
+	t.Run("when provided file without a size", func(t *testing.T) {
+		err := Copy("/dev/urandom", destination, 0, 0)
+
+		require.Equal(t, ErrUnsupportedFile, err)
+	})
+
+	t.Run("when provided file less then specified offset", func(t *testing.T) {
+		err := Copy("testdata/out_offset0_limit10.txt", destination, 100, 0)
+
+		require.Equal(t, ErrOffsetExceedsFileSize, err)
+	})
+
+	t.Run("when limit defined", func(t *testing.T) {
+		matches := "testdata/out_offset0_limit10.txt"
+		err := Copy("testdata/input.txt", destination, 0, 10)
+
+		require.Nil(t, err)
+
+		f1, err := os.Open(matches)
+		require.Nil(t, err)
+		b1, _ := io.ReadAll(f1)
+
+		f2, err := os.Open(destination)
+		require.Nil(t, err)
+		b2, _ := io.ReadAll(f2)
+
+		require.Equal(t, b1, b2)
+	})
+
+	t.Run("when offset and limit defined", func(t *testing.T) {
+		matches := "testdata/out_offset100_limit1000.txt"
+		err := Copy("testdata/input.txt", destination, 100, 1000)
+
+		require.Nil(t, err)
+
+		f1, err := os.Open(matches)
+		require.Nil(t, err)
+		b1, _ := io.ReadAll(f1)
+
+		f2, err := os.Open(destination)
+		require.Nil(t, err)
+		b2, _ := io.ReadAll(f2)
+
+		require.Equal(t, b1, b2)
+	})
 }
