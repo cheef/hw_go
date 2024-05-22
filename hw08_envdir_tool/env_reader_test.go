bab00cd59bcd7d97dbf1a5083604b2bee54149de
Гарматенко Иван Анатольевич <ivan.garmatenko@sbermarket.ru>
2024-05-22 03:10:45 +0300

    HW08 is completed

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("when provided non-existing directory", func(t *testing.T) {
+		env, err := ReadDir("random")
+
+		require.Nil(t, env)
+		require.Equal(t, ErrDirectoryDoesNotExist, err)
+	})
+
+	t.Run("when provided directory with ENV files", func(t *testing.T) {
+		env, err := ReadDir("testdata/env")
+
+		expected := Environment{
+			"BAR":   EnvValue{Value: "bar", NeedRemove: false},
+			"EMPTY": EnvValue{Value: "", NeedRemove: false},
+			"FOO":   EnvValue{Value: "   foo\nwith new line", NeedRemove: false},
+			"HELLO": EnvValue{Value: "\"hello\"", NeedRemove: false},
+			"UNSET": EnvValue{Value: "", NeedRemove: true},
+		}
+
+		require.NoError(t, err)
+		require.Equal(t, expected, env)
+	})
 }
